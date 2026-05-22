9cb646bc91e5f910de0280ee07a79631f503f9ea
Rohit Sutar <rohit1823sutar@gmail.com>
2026-05-22 15:05:50 +0530

    Practiced finding average of array elements in Java

--- src/Arrays/Average.java
+++ src/Arrays/Average.java
@@ -2,9 +2,21 @@ package Arrays;
 
 public class Average {
 
-	public static void main(String[] args) 
+static double getAverage(int [] arr)
 	{
-		System.out.println("Average");
+		double sum=0;
+		int size=arr.length;
+		for(int i=0;i<arr.length;i++)
+		{
+			sum=sum+arr[i];
+		}
+		double avg =sum/size;
+		return avg;
+	}
+public static void main(String[] args) 
+	{
+		int[] arr= {1,2,3,4,5};
+		System.out.println("Average:"+getAverage(arr));
 
 	}
 
