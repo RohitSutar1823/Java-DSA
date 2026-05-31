4957692c90c296f42a8ef174f057b1fcdec5f659
Rohit Sutar <rohit1823sutar@gmail.com>
2026-05-31 14:57:54 +0530

    Practiced LeetCode 1 - Two Sum

---
+++ src/Arrays/Two_Sum.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+public class Two_Sum 
+{
+	static public int[] twoSum(int[] arr,int target)
+	{
+		int n = arr.length;
+		for(int i=0; i<n-1; i++)
+		{
+			for(int j=i+1; j<n; j++)
+			{
+				if(arr[i]+arr[j]==target)
+				{
+					int[] ans = {i,j};
+					return ans;
+				}
+			}
+		}
+		int[] ans= {};
+		return ans;
+	}
+
+	public static void main(String[] args) 
+	{
+		int[] arr = {1,2,3,4,5,6,7,8,9};
+		int[] ans=twoSum(arr, 5);
+		for(int val:ans)
+		{
+			System.out.println(val);
+		}
+
+;
+
+	}
+
+}
