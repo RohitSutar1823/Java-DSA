e0307e7d41e8a37d87e13fdfe6095f5ebf4f8483
Rohit Sutar <rohit1823sutar@gmail.com>
2026-07-24 22:40:34 +0530

    Practiced LeetCode 867 - Transpose Matrix

--- src/Arrays/Transpose_of_a_Matrix.java
+++ src/Arrays/Transpose_of_a_Matrix.java
@@ -1,51 +1,16 @@
-package Arrays;
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-import java.util.Scanner;
-
-public class Transpose_of_a_Matrix {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[][] arr=new int[3][3];
-		System.out.println("Enter matrix element");
-		for(int i = 0; i<3; i++)
-		{
-			for(int j = 0; j<3; j++)
-			{
-				arr[i][j] = sc.nextInt();
-			}
-		}
-		
-		System.out.println("The matrix is:");
-		for(int i = 0; i<3; i++)
-		{
-			for(int j = 0; j<3; j++)
-			{
-				System.out.print(arr[i][j]+" ");
-			}
-			System.out.println();
-		}
-		
-		int ans[][] = new int[3][3];
-		for(int i = 0; i<3; i++)
-		{
-			for(int j = 0; j<3; j++)
-			{
-				ans[j][i] = arr[i][j];
-			}
-		}
-		
-		System.out.println("The Transpose matrix is:");
-		for(int i = 0; i<3; i++)
-		{
-			for(int j = 0; j<3; j++)
-			{
-				System.out.print(ans[i][j]+" ");
-			}
-			System.out.println();
-		}
-		
-
-	}
-
+        int ans[][] = new int[n][m];
+        for(int i = 0; i<m; i++)
+        {
+            for(int j = 0; j<n; j++)
+            {
+                ans[j][i] =  matrix[i][j];
+            }
+        }
+        return ans;
+    }
 }
