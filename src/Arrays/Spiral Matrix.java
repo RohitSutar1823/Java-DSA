28198271f9b68cf2dc054a1e354821ede8cb4c2c
Rohit Sutar <rohit1823sutar@gmail.com>
2026-07-28 19:10:29 +0530

    Practiced LeetCode 54 -Spiral Matrix

---
+++ src/Arrays/Spiral Matrix.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) 
+    {
+        List<Integer> result = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int startingRow = 0;
+        int endingRow = m-1;
+        int startingCol = 0;
+        int endingCol = n-1;
+
+        while(startingRow <= endingRow && startingCol <= endingCol)
+        {
+            // row wise left to right -> startingRow print krnih, from startingCol to endingCol
+            for(int col = startingCol; col<=endingCol; col++)
+            {
+                result.add(matrix[startingRow][col]);
+            }
+            startingRow++;
+            //col wise top to bottom -> endingCol print karna h, from startingRow to endingRow
+            for(int row = startingRow; row<=endingRow; row++)
+            {
+                result.add(matrix[row][endingCol]);
+            }
+            endingCol--;
+            //row wise right to left -> endingRow print krnih, from endingCol to startingCol
+            //valid endingRow -> startingRow endingRow
+            if(startingRow<=endingRow){
+                for(int col = endingCol; col >=startingCol; col--)
+                {
+                    result.add(matrix[endingRow][col]);
+                }
+                endingRow--;
+
+            }
+            //col wise bottom to top -> startingCol print krnah, from endingRow to starttingRow
+            //valid startingCol present h ya nahi
+            if(startingCol<=endingCol){
+                for(int row = endingRow; row>=startingRow; row--)
+                {
+                    result.add(matrix[row][startingCol]);
+                }
+                startingCol++;
+            }
+        }
+        return result;
+    }
+}
