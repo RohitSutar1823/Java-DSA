a39d250c9b1cf231869cbb6ee6192f7393b6e368
Rohit Sutar <rohit1823sutar@gmail.com>
2026-07-31 10:13:17 +0530

    Practiced Bubble Sort

---
+++ src/Sorting Algorithms/Bubble_Sort.java
@@ -0,0 +1,34 @@
+public class Bubble_Sort {
+
+	static void bubbleSort(int[] arr)
+	{
+		int n = arr.length;
+		for(int i = 0; i<n-1; i++)  //rounds
+		{
+			for(int j = 0; j<n-i-1; j++)  //adjacent elements comparison
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		int arr[] = {6,5,1,3};
+		bubbleSort(arr);
+		System.out.println("Printing the array:");
+		for(int value:arr)
+		{
+			System.out.print(value+" ");
+		}
+		
+
+	}
+
+}
