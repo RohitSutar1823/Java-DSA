b5b521211387b85e9123c231b33b43eaf5127015
Rohit Sutar <rohit1823sutar@gmail.com>
2026-08-29 11:47:25 +0530

    Practiced Get Upper Bound

---
+++ src/Binary Search/Get_Upper_Bound.java
@@ -0,0 +1,29 @@
+public class Get_Upper_Bound {
+    public static int getUpperBound(int[] arr, int target){
+        int n = arr.length;
+        int s = 0;
+        int e = n-1;
+        int ans = -1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(arr[mid] <= target){
+                //move to right
+                s = mid + 1;
+            }
+            else{
+                //store ans
+                ans = mid;
+                //move to left
+                e = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 30, 30, 30, 30, 30, 40, 50 };
+        int ans = getUpperBound(arr, 25);
+        System.out.println(ans);
+    }
+}
